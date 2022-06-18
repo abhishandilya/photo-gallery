3f7ad4b687d10ed64db17c25adfca0816977de41
Abhishek Shandilya <ashandilya@greenactionstudio.com>
2022-06-18 19:04:43 -0400

    basic gallery design

--- App.tsx
+++ App.tsx
@@ -1,10 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.topBar}>
+        <Text style={styles.title}>JBF Photo Gallery</Text>
+        <Image style={styles.userImage} source={require("./assets/icon.png")} />
+      </View>
+      <ScrollView>
+        <View style={styles.galleryRow}>
+          <Text style={styles.galleryRowHeader}>June</Text>
+          <View style={styles.imageWrap}>
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+          </View>
+        </View>
+        <View style={styles.galleryRow}>
+          <Text style={styles.galleryRowHeader}>May</Text>
+          <View style={styles.imageWrap}>
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+          </View>
+        </View>
+        <View style={styles.galleryRow}>
+          <Text style={styles.galleryRowHeader}>April</Text>
+          <View style={styles.imageWrap}>
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+            <Image
+              style={styles.galleryImage}
+              source={require("./assets/icon.png")}
+            />
+          </View>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +80,49 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
+    marginHorizontal: 10,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  topBar: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 20,
+  },
+  userImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    // borderColor: "red", // TODO: use theme color
+    // borderWidth: 2,
+  },
+  galleryRow: {
+    borderColor: "black",
+    // borderWidth: 1,
+  },
+  galleryRowHeader: {
+    fontSize: 24,
+    margin: 8,
+    fontWeight: "bold",
+  },
+  imageWrap: {
+    width: "100%",
+    borderColor: "red",
+    // borderWidth: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  galleryImage: {
+    height: 116,
+    width: 116,
+    borderColor: "blue",
+    // borderWidth: 2,
+    margin: 4,
   },
 });
