e7ca108fd20c39dc659591a4e4c18fae065accb9
Abhishek Shandilya <ashandilya@greenactionstudio.com>
2022-06-18 21:02:02 -0400

    added photo screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Gallery from "./screens/Gallery";
+import Photo from "./screens/Photo";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Photo Gallery">
         <Stack.Screen name="Photo Gallery" component={Gallery} />
+        <Stack.Screen name="Photo" component={Photo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Photo.tsx
@@ -0,0 +1,24 @@
+import { Image, StyleSheet, View } from "react-native";
+
+export default function Photo() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../assets/icon.png")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    borderColor: "blue",
+    // borderWidth: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    aspectRatio: 1,
+    height: 350,
+  },
+});
